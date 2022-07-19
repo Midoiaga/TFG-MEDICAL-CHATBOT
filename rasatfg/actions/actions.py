71a31a29f7653368a4db205e0be84be5d14cd2ce
Midoiaga <49227878+Midoiaga@users.noreply.github.com>
2022-07-19 16:29:10 +0200

    zenbakiak kudeatu letrak zenkiakgatik aldatuz

--- rasatfg/actions/actions.py
+++ rasatfg/actions/actions.py
@@ -26,13 +26,67 @@ import es_core_news_sm
 import os
 
 
+def text2int (textnum, numwords={}):
+	if not numwords:
+		units = [
+		"cero", "uno", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho",
+		"nueve", "diez", "once", "doce", "trece", "catorce", "quince",
+		"dieciséis", "diecisiete", "dieciocho", "diecinueve","veinte","veintiuno","veintidós","veintitrés","veinticuatro","veinticinco","veintiséis","veintisiete","veintiocho","veintinueve"
+		]
 
+		tens = ["", "", "", "treinta", "cuarenta", "cincuenta", "sesenta", "setenta", "ochenta", "noventa","cien"]
+
+		scales = ["ciento", "mil", "millon"]
+
+		numwords["y"] = (1, 0)
+		for idx, word in enumerate(units):  numwords[word] = (1, idx)
+		for idx, word in enumerate(tens):       numwords[word] = (1, idx * 10)
+		for idx, word in enumerate(scales): numwords[word] = (10 ** (idx * 3 or 2), 0)
+
+	textnum = textnum.replace('-', ' ')
+
+	current = result = 0
+	curstring = ""
+	onnumber = False
+	for word in textnum.split():
+		if word not in numwords:
+			if onnumber:
+				curstring += repr(result + current) + " "
+			curstring += word + " "
+			result = current = 0
+			onnumber = False
+		else:
+			if word != "y":	
+				scale, increment = numwords[word]
+				current = current * scale + increment
+				if scale > 100:
+					result += current
+					current = 0
+				onnumber = True
+			if word == "y" and onnumber:
+				scale, increment = numwords[word]
+				current = current * scale + increment
+				if scale > 100:
+					result += current
+					current = 0
+				onnumber = True
+			if word == "y" and not onnumber:
+				if onnumber:
+					curstring += repr(result + current) + " "
+				curstring += word + " "
+				result = current = 0
+				onnumber = False
+
+	if onnumber:
+		curstring += repr(result + current)
+
+	return curstring
 
 def _getText() -> Text:
 	info = open("./actions/prueba.txt","r")
 	text = info.read()
 	info.close()
-	return text
+	return text2int(text)
 
 
 # function to preprocess speech
@@ -135,7 +189,7 @@ class ActionFacilitySearch(Action):
 		if age_clean_fin == "":
 			text="Prefiero no decirlo"
 		else:
-			text="Tengo {}".format(age_clean)
+			text="Tengo {}".format(age_clean_fin)
 
 		dispatcher.utter_message(text)
 
